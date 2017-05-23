3fe71ce1bb642dfb9f044895774758303c1069f0
Hwei Tan <chienhwei.tan@gmail.com>
2017-05-23 14:58:46 +0100

    Seeded tasks. Also added completed, delete and add functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :tasks
   #READ
-  get '/tasks', to: 'tasks#index' #get all tasks
-  get '/tasks/:id', to: 'tasks#show' #get a precise task, e.g GET '/tasks/3' get task with id=3
+  # get '/tasks', to: 'tasks#index', as: 'tasks' #get all tasks
+  # get '/tasks/:id', to: 'tasks#show', as: 'task' #get a precise task, e.g GET '/tasks/3' get task with id=3
 
-  #CREATE
-  get '/tasks/new', to: 'tasks#new' #get the form to create a new task
-  post '/tasks', to: 'tasks#create' #post a new task
+  # #CREATE
+  # get '/tasks/new', to: 'tasks#new', as: 'new_task' #get the form to create a new task
+  # post '/tasks', to: 'tasks#create' #post a new task
 
-  #UPDATE
-  get '/tasks/:id/edit', to: 'tasks#edit' #get the form to edit an existing task
-  patch '/tasks/:id', to: 'tasks#update' #update an existing task
+  # #UPDATE
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task' #get the form to edit an existing task
+  # patch '/tasks/:id', to: 'tasks#update' #update an existing task
 
-  #DELETE
-  delete '/tasks/:id', to: 'tasks#destroy' #delete an existing task
+  # #DELETE
+  # delete '/tasks/:id', to: 'tasks#destroy' #delete an existing task
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create!(name: "Flashcards")
+Task.create!(name: "Take Vitamin C")
+Task.create!(name: "Watch Wonder Woman")
